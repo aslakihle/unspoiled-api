163b29520d1dbe258370f8d5ddef5fa361fa7327
Aslak Ihle <aslak.ihle@gmail.com>
2022-03-11 14:09:59 +0100

    vercel

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.use('/user', require('./routes/userRoutes'));
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
 
